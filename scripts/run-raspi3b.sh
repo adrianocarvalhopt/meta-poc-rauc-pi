216520ed3ec06b22938f44be967fe1d0d73b99b5
Adriano Carvalho <adrianocarvalho.pt@gmail.com>
2025-12-18 21:35:26 +0000

    Bugfix

--- scripts/run-raspi3b.sh
+++ scripts/run-raspi3b.sh
@@ -8,6 +8,14 @@ DEPLOY_DIR="tmp/deploy/images/raspberrypi3-64"
 
 KERNEL_FILE="$DEPLOY_DIR/u-boot.bin"
 DTB_FILE="$DEPLOY_DIR/bcm2710-rpi-3-b.dtb"
+BOOTARGS="\
+rw earlyprintk loglevel=8 console=ttyAMA1,115200 console=tty1 \
+coherent_pool=1M 8250.nr_uarts=0 \
+snd_bcm2835.enable_headphones=1 snd_bcm2835.enable_hdmi=1 \
+bcm2708_fb.fbwidth=720 bcm2708_fb.fbheight=480 \
+bcm2708_fb.fbdepth=16 bcm2708_fb.fbswap=1 vc_mem.mem_base=0x3f000000 \
+vc_mem.mem_size=0x3f600000 dwc_otg.lpm_enable=0\
+"
 
 WIC_FILE="$DEPLOY_DIR/core-image-minimal-raspberrypi3-64.rootfs.wic"
 
@@ -47,14 +55,7 @@ args+=(
     -machine raspi3b
     -kernel "$KERNEL_FILE"
     -dtb "$DTB_FILE"
-    -append  "rw earlyprintk loglevel=8 console=ttyAMA1,115200 console=tty1
-              coherent_pool=1M 8250.nr_uarts=0
-              snd_bcm2835.enable_headphones=1 snd_bcm2835.enable_hdmi=1
-              bcm2708_fb.fbwidth=720 bcm2708_fb.fbheight=480
-              bcm2708_fb.fbdepth=16 bcm2708_fb.fbswap=1 vc_mem.mem_base=0x3f000000
-              vc_mem.mem_size=0x3f600000 dwc_otg.lpm_enable=0
-              root=/dev/mmcblk0p2 rootfstype=ext4 rootdelay=1 fsck.repair=yes
-              verbosity=2 net.ifnames=0"
+    -append "$BOOTARGS"
 
 # Other kernel cmdline options:
 # dwc2 network
--- scripts/run-raspi4b.sh
+++ scripts/run-raspi4b.sh
@@ -8,6 +8,14 @@ DEPLOY_DIR="tmp/deploy/images/raspberrypi4-64"
 
 KERNEL_FILE="$DEPLOY_DIR/u-boot.bin"
 DTB_FILE="$DEPLOY_DIR/bcm2711-rpi-4-b.dtb"
+BOOTARGS="\
+rw earlyprintk loglevel=8 console=ttyAMA1,115200 console=tty1 \
+coherent_pool=1M 8250.nr_uarts=0 \
+snd_bcm2835.enable_headphones=1 snd_bcm2835.enable_hdmi=1 \
+bcm2708_fb.fbwidth=720 bcm2708_fb.fbheight=480 \
+bcm2708_fb.fbdepth=16 bcm2708_fb.fbswap=1 vc_mem.mem_base=0x3f000000 \
+vc_mem.mem_size=0x3f600000 dwc_otg.lpm_enable=0\
+"
 
 WIC_FILE="$DEPLOY_DIR/core-image-minimal-raspberrypi4-64.rootfs.wic"
 
@@ -47,14 +55,7 @@ args+=(
     -machine raspi4b
     -kernel "$KERNEL_FILE"
     -dtb "$DTB_FILE"
-    -append  "rw earlyprintk loglevel=8 console=ttyAMA1,115200 console=tty1
-              coherent_pool=1M 8250.nr_uarts=0
-              snd_bcm2835.enable_headphones=1 snd_bcm2835.enable_hdmi=1
-              bcm2708_fb.fbwidth=720 bcm2708_fb.fbheight=480
-              bcm2708_fb.fbdepth=16 bcm2708_fb.fbswap=1 vc_mem.mem_base=0x3f000000
-              vc_mem.mem_size=0x3f600000 dwc_otg.lpm_enable=0
-              root=/dev/mmcblk1p2 rootfstype=ext4 rootdelay=1 fsck.repair=yes
-              verbosity=2 net.ifnames=0"
+    -append "$BOOTARGS"
 
 # Other kernel cmdline options:
 # dwc2 network
